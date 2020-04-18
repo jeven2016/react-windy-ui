6f7283b7c2499c05b6b0e85ece2e153d993528aa
jujucom@126.com <jujucom@126.com>
2020-04-18 13:08:31 +0800

    Revert "update"
    This reverts commit 4aca5158f82f5ff6ba964e23f087c575d4f8be10.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Button} from 'react-windy-ui';
+import {Button} from './react-wui-modern';
 
 function App() {
   return (
