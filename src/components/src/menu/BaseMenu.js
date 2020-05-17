410c54d92b7479e3538605d053c90a42230761be
jujucom@126.com <jujucom@126.com>
2020-05-17 21:34:30 +0800

    update doc component

---
+++ src/components/src/menu/BaseMenu.js
@@ -0,0 +1,155 @@
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+import clsx from 'clsx';
+import MenuHeader from './MenuHeader';
+import {Action, MenuDirection} from './MenuUtils';
+import MenuList from './MenuList';
+import useMultipleRefs from '../common/UseMultipleRefs';
+import {MenuContext} from '../common/Context';
+import {execute, includes, isNil} from '../Utils';
+import PropTypes from 'prop-types';
+
+/**
+ * SubMenu Component
+ */
+const BaseMenu = React.forwardRef((props, ref) => {
+  const {
+    id,
+    className,
+    children,
+    icon,
+    popupSubMenu = false,
+    popupSubMenuPostion = 'right',
+    blockList = false,
+    rootMenu = false,
+    hasBottomBar = false,
+    ...otherProps
+  } = props;
+  const ctx = useContext(MenuContext);
+  const {attach, detach, getState} = ctx.store;
+
+  //only works for non popup submenu and the id is not specified
+  const [collapse, setCollapse] = useState(null);
+
+  const internalRef = useRef(null);
+  const multiRef = useMultipleRefs(ref, internalRef);
+  const [open, setOpen] = useState(includes(getState().openList, id));
+
+  //handle collapsable submenu
+  const collapseHandler = useCallback((e) => {
+    if (isNil(id)) {
+      setCollapse(pre => { setCollapse(!pre); });
+      return;
+    }
+    //the id should exists in the list ,and then it can be removed that means to collapse the panel
+    const toCollapse = getState().openList.includes(id);
+
+    const activeType = toCollapse ? Action.closeMenu : Action.openMenu;
+    ctx.dispatch({type: activeType, id, e, directChild: ctx.directChild});
+  }, [ctx, id, getState]);
+
+  //handle popup submenu
+  useEffect(() => {
+    if (rootMenu || isNil(id)) {
+      return;
+    }
+    const listener = ({openList}) => {
+      const nextOpen = includes(openList, id);
+      //to open
+      if (nextOpen && !open) {
+        setOpen(true);
+      }
+
+      //to close
+      if (!nextOpen && open) {
+        setOpen(false);
+      }
+    };
+    attach(listener);
+    return () => {
+      detach(listener);
+    };
+  }, [id, open, setOpen, rootMenu, attach, detach]);
+
+  const timeoutRef = useRef(null);
+  const mouseEnterHandler = useCallback((e) => {
+    if (!popupSubMenu) {
+      return;
+    }
+    const closeTimeout = timeoutRef.current;
+    if (!isNil(closeTimeout)) {
+      timeoutRef.current = null;
+      clearTimeout(closeTimeout);
+    }
+    ctx.dispatch({type: Action.openMenu, id, e, directChild: ctx.directChild});
+  }, [ctx, id, popupSubMenu]);
+
+  const mouseLeaveHandler = useCallback((e) => {
+    if (!popupSubMenu) {
+      return;
+    }
+
+    //delay some mill-seconds to let mouse enter handler be invoked first
+    timeoutRef.current = execute(function() {
+      ctx.dispatch({type: Action.closeMenu, id, e});
+    }, 50);
+
+  }, [ctx, id, popupSubMenu]);
+
+  const directionCls = MenuDirection.isVertical(ctx.direction)
+      ? MenuDirection.vertical.className
+      : MenuDirection.horizontal.className;
+
+  const clsName = clsx(className, directionCls, {
+    compact: ctx.canCompact && ctx.compact,
+    'non-compact': ctx.canCompact && !ctx.compact,
+    'global-with-box': ctx.hasBox,
+    'with-border-radius': ctx.hasBorderRadius,
+    [ctx.type]: ctx.type,
+  });
+
+  return <>
+    <div ref={multiRef}
+         className={clsName} {...otherProps}>
+      {ctx.hasHeader && <MenuHeader icon={icon}
+                                    handleCollapse={collapseHandler}
+                                    handleMouseEnter={mouseEnterHandler}
+                                    handleMouseLeave={mouseLeaveHandler}
+                                    menuVisible={open}
+                                    hasBottomBar={hasBottomBar}
+                                    collapse={isNil(collapse)
+                                        ? !open
+                                        : collapse}/>}
+
+      <MenuList
+          popupSubMenu={popupSubMenu}
+          popupSubMenuPostion={popupSubMenuPostion}
+          collapse={isNil(collapse) ? !open : collapse}
+          content={children}
+          handleMouseEnter={mouseEnterHandler}
+          handleMouseLeave={mouseLeaveHandler}
+          show={open}
+          blockList={blockList}
+      />
+
+    </div>
+  </>;
+});
+
+BaseMenu.propTypes = {
+  id: PropTypes.string,
+  className: PropTypes.string,
+  icon: PropTypes.node,
+  popupSubMenu: PropTypes.bool,
+  popupSubMenuPostion: PropTypes.string,
+  blockList: PropTypes.bool,
+  rootMenu: PropTypes.bool,
+  hasBottomBar: PropTypes.bool,
+};
+
+export default BaseMenu;
---
+++ src/components/src/menu/SubMenu.js
@@ -0,0 +1,82 @@
+import React, {useContext, useMemo} from 'react';
+import BaseMenu from './BaseMenu';
+import clsx from 'clsx';
+import {MenuContext} from '../common/Context';
+import {MenuDirection, SubMenuDirection} from './MenuUtils';
+import PropTypes from 'prop-types';
+
+/**
+ * SubMenu Component
+ */
+const SubMenu = React.forwardRef((props, ref) => {
+  const {
+    id,
+    hasBottomBar,
+    className = 'base-menu',
+    header,
+    extraClassName,
+    directChild = false, //is this submenu the child of Menu
+    ...otherProps
+  } = props;
+  const ctx = useContext(MenuContext);
+  const clsName = clsx(extraClassName, className, {
+    'popup-menu': ctx.popupSubMenu,
+  });
+
+  const isCollapsable = useMemo(
+      () => ctx.popupSubMenu ? false : ctx.collapsable,
+      [ctx]);
+
+  const subCtx = {
+    ...ctx,
+    directChild,
+    header: header,
+    hasBox: ctx.popupSubMenu && !ctx.popupSubMenu,
+    canCompact: directChild,
+    collapsable: isCollapsable,
+    direction: MenuDirection.vertical.key,
+    hasHeader: true,
+  };
+
+  //the direct child of menu should align the botom of the menu, others should  align right
+  const popupSubMenuPostion = useMemo(() => {
+    if (!ctx.popupSubMenu) {
+      return null;
+    }
+
+    //for horizontal, the direct child should display under the header
+    if (!MenuDirection.isVertical(ctx.direction)
+        && directChild) {
+      return SubMenuDirection.bottom.key;
+    }
+    return SubMenuDirection.right.key;
+  }, [ctx, directChild]);
+
+  //if it is a direct child the width should be set to 'block' otherwise just ignore it
+  const blockList = SubMenuDirection.isBottom(popupSubMenuPostion);
+
+  return <MenuContext.Provider value={subCtx}>
+    <BaseMenu className={clsName}
+              hasBottomBar={hasBottomBar}
+              ref={ref}
+              id={id}
+              rootMenu={false}
+              popupSubMenu={ctx.popupSubMenu} //the submenu can pops the items list
+              popupSubMenuPostion={popupSubMenuPostion}
+              blockList={blockList}
+              {...otherProps} />
+  </MenuContext.Provider>;
+});
+
+SubMenu.propTypes = {
+  id: PropTypes.string,
+  className: PropTypes.string,
+  icon: PropTypes.node,
+  popupSubMenu: PropTypes.bool,
+  popupSubMenuPostion: PropTypes.string,
+  blockList: PropTypes.bool,
+  rootMenu: PropTypes.bool,
+  hasBottomBar: PropTypes.bool,
+};
+
+export default SubMenu;
