a3aac3ac8cb842faffc6cc9413aa43f5fdd39fed
jujucom <jujucom@126.com>
2021-07-27 22:04:06 +0800

    finished list

--- packages/docs/src/pages/skeleton/samples/Skeleton3.js
+++ packages/docs/src/pages/skeleton/samples/Skeleton3.js
@@ -3,45 +3,43 @@ import {Avatar, Card, List, Skeleton, Toggle} from "react-windy-ui";
 import pic from '../../../style/imgs/pic_p2.jpg'
 
 function SkeletonComponent({loading}) {
-  return loading ? <Card style={{width: '20rem'}} hasBox={false}>
-    <Card.Row>
-      <Skeleton>
-        <Skeleton.Item height={150}/>
-        <List style={{marginTop: '1rem'}}>
-          <List.Item
-            compact
-            type="panel"
-            icon={<Skeleton.Item type="circle"/>}
-            title={<Skeleton.Item width='40%' height={12}/>}
-            description={<Skeleton.Item height={12}/>}
-          />
-        </List>
-        <Skeleton.Item height={12}/>
-        <Skeleton.Item height={12}/>
-      </Skeleton>
-    </Card.Row>
-  </Card> : <Card style={{width: '20rem'}}>
-    <Card.CardImage autoScale>
-      <Card.Image src={pic} style={{height: '150px'}}>
-      </Card.Image>
-    </Card.CardImage>
-    <Card.Body>
-      <List>
+  return loading ?
+    <Skeleton style={{width: '20rem'}}>
+      <Skeleton.Item height={150}/>
+      <List style={{marginTop: '1rem'}}>
         <List.Item
           compact
-          vertical={true}
           type="panel"
-          icon={<Avatar hasBox={false} extraClassName="bg-color-green">B</Avatar>}
-          title={<h5>Wang</h5>}
-          description={<h6>The description</h6>}>
-          <h6 className="text comment">A hill is a landform that extends above the surrounding terrain. It often has a
-            distinct summit.
-          </h6>
-        </List.Item>
+          icon={<Skeleton.Item type="circle"/>}
+          title={<Skeleton.Item width='40%' height={12}/>}
+          description={<Skeleton.Item height={12}/>}
+        />
       </List>
-    </Card.Body>
+      <Skeleton.Item height={12}/>
+      <Skeleton.Item height={12}/>
+    </Skeleton>
+    : <Card style={{width: '20rem'}}>
+      <Card.CardImage autoScale>
+        <Card.Image src={pic} style={{height: '150px'}}>
+        </Card.Image>
+      </Card.CardImage>
+      <Card.Body>
+        <List>
+          <List.Item
+            compact
+            vertical={true}
+            type="panel"
+            icon={<Avatar hasBox={false} extraClassName="bg-color-green">B</Avatar>}
+            title={<h5>Wang</h5>}
+            description={<h6>The description</h6>}>
+            <h6 className="text comment">A hill is a landform that extends above the surrounding terrain. It often has a
+              distinct summit.
+            </h6>
+          </List.Item>
+        </List>
+      </Card.Body>
 
-  </Card>;
+    </Card>;
 }
 
 
