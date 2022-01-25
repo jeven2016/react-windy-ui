d8b97095a4a6ec90f16b6fa0d01fd9fe1c319e46
jujucom <jujucom@126.com>
2022-01-25 20:04:18 +0800

    add card ts

---
+++ packages/components/src/card/Card.d.ts
@@ -0,0 +1,11 @@
+export interface BaseButtonProps {
+  children?: React.ReactNode;
+  className?: string;
+  extraClassName?: string;
+  block?: boolean;
+  autoScale?: boolean;
+  hasBorder?: boolean;
+  hasBox?: boolean;
+  hasWidth?: boolean;
+  rise?: boolean;
+}
