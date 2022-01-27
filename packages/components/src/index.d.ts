92ec98557aba1308ac323d19cfe0da8a8d4176f9
jujucom <jujucom@126.com>
2022-01-27 17:54:34 +0800

    add input definition file

--- packages/components/src/index.d.ts
+++ packages/components/src/index.d.ts
@@ -1,1 +1,2 @@
-export { default as Button, ButtonProps } from './button';
+export type {};
+export { default as Button } from './button';
---
+++ packages/components/src/input.d.ts
@@ -0,0 +1,11 @@
+export interface BaseInputProps {
+  size?: string;
+  type?: string;
+  disabled?: boolean;
+  block?: boolean;
+  hasBox?: boolean;
+  borderType;
+  readOnly?: boolean;
+  canFocus?: boolean;
+  errorType?: string;
+}
