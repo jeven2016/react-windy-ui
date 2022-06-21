af3f42ecea00704ad174af1f5e026621e8f4ca32
jujucom <jujucom@126.com>
2022-06-21 20:25:37 +0800

    revert to public registry

--- packages/components/src/card/Card.d.ts
+++ packages/components/src/card/Card.d.ts
@@ -1,10 +1,11 @@
-import React from 'react';
-import { ClassNameRefProps, CommonProps } from '../generic';
-import Curtain from './Curtain';
+import React from "react";
+import { ClassNameRefProps, CommonProps } from "../generic";
+import Curtain from "./Curtain";
 
 export type CardProps = {
   block?: boolean;
   autoScale?: boolean;
+  rise?: boolean;
   hasBorder?: boolean;
   hasBox?: boolean;
   hasWidth?: boolean;
